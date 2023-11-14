520ab911be689aaf0f49b55b422391a9a44f964c
Walterhart <walterwhart1@gmail.com>
2023-11-14 16:34:55 -0500

    Complete challenge

--- App.jsx
+++ App.jsx
@@ -18,54 +18,29 @@ Two DiaryCard components are currently hardcoded with data below. Your task is t
         
     3. Make the code concise. 
 */
-    
+    // iterate through diaryData and display DiaryCard for each data
+    const diaryCardElement = diaryData.map((diary, index) =>(
+        <DiaryCard
+         key= {diary.id}
+         {...diary}   
+        />
+        )
+    )
+   
     return (
         <div>
             <Menu skip={false}/>
+            
             <main className="main-container">
                 <div className="sub-header">
                     <h3>April Reports</h3>
                 </div>
                 
-                <DiaryCard
-                    key="this is a temporary key"
-                    date="April 1"
-                    rating={77}
-                    emotions={[
-                        { icon: "😊", text: "Satisifed" },
-                        { icon: "☺️", text: "Relaxed" },
-                        { icon: "🙏", text: "Grateful" },
-                        { icon: "️️️⚡️", text: "Energetic" }
-                    ]}
-                    activities={[
-                        { icon: "🏃‍♂️", text: "Running" },
-                        { icon: "️️️🧘‍♂️", text: "Meditation" },
-                        { icon: "️✍️", text: "Journaling" },
-                        { icon: "️📖", text: "Reading" },
-                        { icon: "️👨‍👩‍👦‍👦", text: "Family Time" }
-                    ]}
-                    note={`Nothing very exciting happened, but today was a pretty nice day! I had a nice balance of "me time" and family time, and I felt very relaxed and satisfied by the time I went to bed.`}
-                    />
-                    
-                <DiaryCard
-                    key="this is just another temporary key"
-                    date="April 2"
-                    rating={96}
-                    emotions={[
-                        { icon: "😇", text: "Proud" },
-                        { icon: "🥰", text: "Appreciated" },
-                        { icon: "😃", text: "Joyful" },
-                        { icon: "️🥳", text: "Celebratory" }
-                    ]}
-                    activities={[
-                        { icon: "🏆", text: "Personal Victory" },
-                        { icon: "️️️💪", text: "Personal Growth" },
-                        { icon: "️🍛", text: "Good Meal" },
-                        { icon: "️🛌", text: "Good Sleep" },
-                        { icon: "️👨‍👩‍👦‍👦", text: "Family Time" }
-                    ]}
-                    note={`"I finally finished my big work project that I've been working on for the past three months, and I feel like I've really improved myself as a member of my team."`}
-                    />
+                {diaryData.length > 0? (
+                    diaryCardElement
+                ):(
+                     <p>No diary entries found.</p>
+                )}
                     
             </main>
         </div>
